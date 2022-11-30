f3b94e694970c21a7052452f051d7be0f614809c
Muhammad Hassan Javed <80898851+hassan028@users.noreply.github.com>
2022-11-30 12:29:59 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# FLSD_App
+# Skin-Disease-Classification-Efficient-Net-b2
+This repo contains the skin disease classifier that consists on 24 sikn disease. A running application can be accessed [here](https://abdulrafay97-skin-disease-classification-efficient-n-app-qmw5x9.streamlitapp.com/). 
---
+++ app.py
@@ -0,0 +1,41 @@
+#Import Packages
+import numpy as np
+from PIL import Image
+import streamlit as st
+from io import BytesIO
+import base64
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
+
+disable_warnings(InsecureRequestWarning)
+
+icon = Image.open('icon.jpeg')
+st.set_page_config(page_title='Classifier', page_icon = icon)
+st.header('Face Liveness Spoofing Detection')
+st.sidebar.subheader(("Input a Face Picture"))
+
+#Get Image
+file_up = st.sidebar.file_uploader('Upload an Image', type = "jpeg")
+
+def img_to_bytes(img):
+    buffered = BytesIO()
+    img.save(buffered, format=img.format)
+    img_str = base64.b64encode(buffered.getvalue())
+    strIMG = img_str.decode("utf-8")
+    resp = get_response(strIMG)
+    return resp
+
+def get_response(ImgStr):
+    url = 'https://27030819-823c-42a3-ab8a-3e05ca9185df.syndic.ai'
+    data = {'url': ImgStr}
+
+    x = requests.post(url, data, verify=False)
+    return x.text
+
+if file_up is not None:
+    # display image that user uploaded
+    image = Image.open(file_up)
+    result = img_to_bytes(image)
+    st.image(image, caption = 'Original Image', width=550)
+    st.write("Predictions: ",result)
